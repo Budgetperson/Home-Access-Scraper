e4d4d77422997cdd50bafa23d5568e611157f654
Yash Aggarwal <yash0609@gmail.com>
2012-02-08 20:35:22 -0600

    removed time-tracking stuff

--- driver.php
+++ driver.php
@@ -1,16 +1,9 @@
 <?php
-function microtime_float()
-{
-    list($usec, $sec) = explode(" ", microtime());
-    return ((float)$usec + (float)$sec);
-}
-$time_start = microtime_float();
+
 
 require_once("hacoo.php");
 echo "<pre>";
 $me = new HAC(/* your studentid get variable*/);
 print_r($me->courses);
-$time_end = microtime_float();
-$time = $time_end - $time_start;
-echo "Did nothing in $time seconds\n";
+
 echo memory_get_peak_usage();
